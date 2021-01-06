c844dc6945503c7b957a165b731bb6c8f724a287
Dmitry Chuev <d.chuev@propellerads.net>
2021-01-06 16:39:51 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/com/sleep-cat/gogreet-example
+
+go 1.15
+
+require github.com/sleepy-cat/gogreet v0.0.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sleepy-cat/gogreet"
+)
+
+func main() {
+	fmt.Println(greet.Greet("World"))
+}
